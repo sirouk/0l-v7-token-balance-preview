335f8ee623268ad3be1ff8515c63250cfa366027
sirouk <8901571+sirouk@users.noreply.github.com>
2023-11-01 13:19:32 -0400

    more is less

--- pages/index.js
+++ pages/index.js
@@ -34,25 +34,13 @@ const HomePage = () => {
       </div>
       {data && (
         <div>
-          <h2>Jail Information</h2>
-          <p>Is Jailed: {String(extractData("0x1::jail::Jail").is_jailed)}</p>
-          <p>Lifetime Vouchees Jailed: {extractData("0x1::jail::Jail").lifetime_vouchees_jailed}</p>
-
+          
           <h2>Account Details</h2>
           <p>Sequence Number: {extractData("0x1::account::Account").sequence_number}</p>
           <p>Authentication Key: {extractData("0x1::account::Account").authentication_key}</p>
-
-          <h2>Validator Details</h2>
-          <p>Consensus Public Key: {extractData("0x1::stake::ValidatorConfig").consensus_pubkey}</p>
-          <p>Validator Index: {extractData("0x1::stake::ValidatorConfig").validator_index}</p>
-
+          
           <h2>Balance</h2>
           <p>Current Balance: {extractData("0x1::coin::CoinStore<0x1::gas_coin::LibraCoin>").coin.value}</p>
-
-          <h2>Tower State</h2>
-          <p>Contiguous Epochs Mining: {extractData("0x1::tower_state::TowerProofHistory").contiguous_epochs_mining}</p>
-          <p>Epochs Mining: {extractData("0x1::tower_state::TowerProofHistory").epochs_mining}</p>
-          <p>Latest Epoch Mining: {extractData("0x1::tower_state::TowerProofHistory").latest_epoch_mining}</p>
 
           <h2>Slow Wallet</h2>
           <p>Transferred: {extractData("0x1::slow_wallet::SlowWallet").transferred}</p>
