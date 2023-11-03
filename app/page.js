f77d77247ded7b26e99039c879f8b9b5be59c82f
sirouk <8901571+sirouk@users.noreply.github.com>
2023-11-02 23:18:02 -0400

    edge runtime

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"; // This is a client component
 import Image from 'next/image'
-export const runtime = "experimental-edge"
+export const runtime = 'edge';
 import { useState } from 'react';
 
 const HomePage = () => {
